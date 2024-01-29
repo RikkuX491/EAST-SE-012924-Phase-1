5a83948d39901f9ab57f3e66f9a8dfdf5c654d17
Ricardo Guerra <RikkuG491@gmail.com>
2024-01-29 18:01:27 -0500

    Added solution code for Deliverables # 2 - 7

--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@
 
 // Deliverable # 1
 const greeting = "Welcome to Flatburger!"
+
+// Deliverable # 2
+let num1 = 7
+let num2 = 5
+
+// Deliverable # 3
+const sum = num1 + num2
+
+// Deliverable # 4
+const sumString = `${num1} + ${num2} = ${sum}`
+
+// Deliverable # 5
+if(num1 === 7 || num1 === 49){
+    console.log("That's a lucky number!")
+}
+
+// Deliverable # 6
+else{
+    console.log("That's not a lucky number.")
+}
+
+// Deliverable # 7
+num1 === 7 && num2 === 7 ? "Lucky Sevens!" : "Better luck next time"
