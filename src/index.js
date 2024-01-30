df45d697b54e4fd00374d6708710a9b2891195e5
Ricardo Guerra <RikkuG491@gmail.com>
2024-01-30 18:03:33 -0500

    Added solution code for Deliverables # 3 & 4

--- src/index.js
+++ src/index.js
@@ -5,12 +5,31 @@ function flatburgerGreeting(){
     console.log("Welcome to Flatburger!")
 }
 
-flatburgerGreeting()
-
 // Deliverable # 2
 function printGreeting(greeting){
     console.log(greeting)
 }
 
+// Deliverable # 3
+const getSum = (num1, num2) => {
+    return num1 + num2
+}
+
+// Deliverable # 4
+function getSumString(sum, num1, num2){
+    return `${num1} + ${num2} = ${sum(num1, num2)}`
+}
+
+// Testing - Calling the function from Deliverable # 1
+flatburgerGreeting()
+
+// Testing - Calling the function from Deliverable # 2
 printGreeting("Hello!")
-printGreeting("Goodbye!")
+
+// Testing - Calling the function from Deliverable # 3
+const sumValue = getSum(7, 14)
+console.log(sumValue)
+
+// Testing - Calling the function from Deliverable # 4
+const sumString = getSumString(getSum, 8, 15)
+console.log(sumString)
