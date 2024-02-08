e2e20f99518dc3eac82c70f9da9105516028132f
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-08 12:18:44 -0500

    Added lecture code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+let currentlyDisplayedFood = null
+let foodsArray = null
+
 const restaurantMenu = document.getElementById('restaurant-menu')
 
 fetch('http://localhost:3000/foods')
 .then(response => response.json())
 .then(foods => {
+    foodsArray = foods
     displayFoodDetails(foods[0])
 
     foods.forEach(food => {
@@ -13,13 +17,39 @@ fetch('http://localhost:3000/foods')
 function addFoodImageToRestaurantMenu(food){
     const imgElement = document.createElement('img')
     imgElement.src = food.image
+    
     imgElement.addEventListener('mouseover', () => {
         displayFoodDetails(food)
     })
+
+    imgElement.addEventListener('click', () => {
+        fetch(`http://localhost:3000/foods/${currentlyDisplayedFood.id}`, {
+            method: "DELETE"
+        })
+        .then(response => {
+            if(response.ok){
+                // imgElement.remove()
+
+                foodsArray = foodsArray.filter(food => {
+                    return food.id !== currentlyDisplayedFood.id
+                })
+
+                updateFoodsInRestaurantMenu()
+            }
+            else{
+                alert("Error: Unable to delete food!")
+            }
+        })
+        .catch(error => {
+            alert("Error: Unable to delete food!")
+        })
+    })
+
     restaurantMenu.appendChild(imgElement)
 }
 
 function displayFoodDetails(food){
+    currentlyDisplayedFood = food
     const foodDetailImageElement = document.getElementsByClassName('detail-image')[0]
     foodDetailImageElement.src = food.image
     const foodNameElement = document.getElementsByClassName('name')[0]
@@ -55,7 +85,10 @@ newFoodForm.addEventListener('submit', (event) => {
     .then(response => {
         if(response.ok){
             response.json().then(newFoodData => {
-                addFoodImageToRestaurantMenu(newFoodData)
+                // addFoodImageToRestaurantMenu(newFoodData)
+                foodsArray.push(newFoodData)
+
+                updateFoodsInRestaurantMenu()
             })
         }
         else{
@@ -72,7 +105,46 @@ addToCartForm.addEventListener('submit', (event) => {
     const numberToAddInputElement = document.getElementById('number-to-add')
     const numberInCartCountElement = document.getElementById('number-in-cart-count')
     const sum = Number(numberInCartCountElement.textContent) + Number(numberToAddInputElement.value)
-    numberInCartCountElement.textContent = sum
+
+    fetch(`http://localhost:3000/foods/${currentlyDisplayedFood.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            number_in_cart: sum
+        })
+    })
+    .then(response => {
+        if(response.ok){
+            response.json().then(updatedFood => {
+                numberInCartCountElement.textContent = sum
+
+                foodsArray = foodsArray.map(food => {
+                    if(food.id === updatedFood.id){
+                        return updatedFood
+                    }
+                    else{
+                        return food
+                    }
+                })
+
+                updateFoodsInRestaurantMenu()
+            })
+        }
+        else{
+            alert("Error: Unable to add to cart!")
+        }
+    })
 
     addToCartForm.reset()
 })
+
+function updateFoodsInRestaurantMenu(){
+    restaurantMenu.innerHTML = ""
+
+    foodsArray.forEach(food => {
+        addFoodImageToRestaurantMenu(food)
+    })
+}
